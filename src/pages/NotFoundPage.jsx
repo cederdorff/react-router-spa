03b6073cf5aecc801913feca5ce78a9ff74e6fe7
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-02-23 18:57:47 +0100

    feat: enhance NotFoundPage layout with semantic elements and improved styles

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,10 +2,16 @@ import { Link } from "react-router";
 
 export default function NotFoundPage() {
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <p>Sorry, the page you're looking for doesn't exist.</p>
-      <Link to="/">Go back home</Link>
-    </div>
+    <>
+      <header>
+        <h1 className="not-found-title">404</h1>
+      </header>
+      <main className="not-found">
+        <p>Sorry, the page you're looking for doesn't exist.</p>
+        <Link to="/" className="not-found-link">
+          Go back home
+        </Link>
+      </main>
+    </>
   );
 }
