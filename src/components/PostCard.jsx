4f85617e009ad25e04f1ef7b2bfbb27a6c250887
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-05-26 11:23:39 +0200

    feat: format post creation date in PostCard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,10 +1,15 @@
 export default function PostCard({ post }) {
+  const formattedDate = new Date(post.created_at).toLocaleString("da-DK", {
+    dateStyle: "long",
+    timeStyle: "short"
+  });
+
   return (
     <article className="post-card">
       <img className="post-image" src={post.image} alt="" />
       <div className="post-card-content">
         <time className="post-time" dateTime={post.created_at}>
-          {post.created_at}
+          {formattedDate}
         </time>
         <h2>{post.caption}</h2>
       </div>
